dfad770fc065314cd6c9797552ae14f5d1f11c44
luciamam <lumanmba2023@gmail.com>
2025-02-28 11:35:03 +0100

    conexion a la base  a mongo registrando al usuario , perfectamente

--- src/app.py
+++ src/app.py
@@ -1,9 +1,19 @@
-from flask import Flask,render_template, redirect, request
+from flask import Flask,render_template, redirect, request,url_for,flash
 from flask_bootstrap import Bootstrap4 
 from dotenv import load_dotenv
 from formularios.forms import RegisterUser,LoginUser
+from pymongo import MongoClient
 import os
 load_dotenv
+
+
+
+#AQUI ES DONDE REALIZAMOS LA CONEXION A MONGODB 
+client = MongoClient("localhost", 27017)
+#accedemos a  la base de datos ,aunque si en mogo no se encuentra la crea 
+db = client['MibasedeDatosUsuarios']
+#creamos la collecction 
+users_collection=db['usuarios']
 
 
 
@@ -20,9 +30,26 @@ def home():
     return render_template('Home.html')
 
 
-@app.route('/register')
+@app.route('/register',methods=['GET','POST'])
 def register():
     form=RegisterUser()
+    datos=request.form
+
+    if request.method=='POST':
+        #vamos a segurarnos que el formualrio este validaddo , en caso contrario que no se puede enviar estos datos 
+        if not form.validate_on_submit():
+            return redirect(url_for('register'))
+        else:
+            #guardamos los datos de nuestro usuario a la base de datos 
+            usuario={
+                'username':datos['username'],
+                'password':datos['password'],
+                'fullname':datos['fullname']
+            }
+            users_collection.insert_one(usuario)
+            return redirect(url_for('perfil'))
+            
+
     return render_template('RegistrarUsuario.html',form=form)
 
 
@@ -38,6 +65,7 @@ def pagina_404(error):
     return "<h1> Este recurso no se encuentra 😎</h1>"
 
 
+#la ruta perfil tiene que esta protegida en esste caso lo vamos a proteger con el token jwt 
 @app.route('/perfil')
 def perfil():
     return "bienvenido usuario "
--- src/formularios/forms.py
+++ src/formularios/forms.py
@@ -3,14 +3,14 @@ from wtforms import StringField, EmailField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 
 class RegisterUser(FlaskForm):
-    username=StringField("username",validators=[DataRequired(),Length(min=6,max=12)])
+    username=StringField("username",validators=[DataRequired(),Length(min=4,max=12)])
     fullname=StringField("fullname",validators=[DataRequired(),Length(min=6,max=20)])
-    password=StringField("password",validators=[DataRequired(),Length(min=6,max=20)])
+    password=PasswordField("password",validators=[DataRequired(),Length(min=6,max=20)])
     submit=SubmitField("Registrarse")
     
 class LoginUser(FlaskForm):
-     username=StringField("username",validators=[DataRequired(),Length(min=6,max=12)])
-     password=StringField("password",validators=[DataRequired(),Length(min=6,max=20)])
+     username=StringField("username",validators=[DataRequired(),Length(min=4,max=12)])
+     password=PasswordField("password",validators=[DataRequired(),Length(min=6,max=20)])
      Submit=SubmitField('Inciar Sesion')
 
 
